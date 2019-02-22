72b418b4a8f7ad503ae3d103b2fb6c8be696c9a6
Adesire <desilere@gmail.com>
2019-02-22 01:04:47 +0100

    Finished layout

--- app/src/main/java/com/overcomersprayers/app/overcomersprayers/PrayerHeadingActivity.java
+++ app/src/main/java/com/overcomersprayers/app/overcomersprayers/PrayerHeadingActivity.java
@@ -3,11 +3,11 @@ package com.overcomersprayers.app.overcomersprayers;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PrayerHeadingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_prayer_heading);
     }
 }
