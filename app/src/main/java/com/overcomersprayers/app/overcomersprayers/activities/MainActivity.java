5792536d189b9f171a54e98425df5b91b878cecf
Oluwatayo <oluwatayoadegboye@gmail.com>
2019-03-12 13:37:53 +0100

    wip

--- app/src/main/java/com/overcomersprayers/app/overcomersprayers/activities/MainActivity.java
+++ app/src/main/java/com/overcomersprayers/app/overcomersprayers/activities/MainActivity.java
@@ -18,7 +18,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
